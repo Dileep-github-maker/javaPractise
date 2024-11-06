b8bfc5b883eafcfd49ad7f4bde0c1026a8b39f2b
dileep.lokesh <dileepdharmavaram@gmail.com>
2024-11-06 22:09:52 +0530

    iam there

---
+++ one.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class one{
+    public static void main(String[] a){
+        ArrayList<Integer> obj=new ArrayList<>();
+        obj.addFirst(1);
+        System.out.println(obj);
+    }
+}
